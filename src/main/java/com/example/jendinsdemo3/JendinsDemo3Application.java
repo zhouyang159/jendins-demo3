36b104cd345bf2d4a91dbb40c8364c995bf07f8d
zhouyang <602482790@qq.com>
2024-03-22 00:57:14 +0800

    post hi commit

--- src/main/java/com/example/jendinsdemo3/JendinsDemo3Application.java
+++ src/main/java/com/example/jendinsdemo3/JendinsDemo3Application.java
@@ -3,6 +3,7 @@ package com.example.jendinsdemo3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -18,4 +19,8 @@ public class JendinsDemo3Application {
 		return "Hello DK";
 	}
 
+	@PostMapping("/hi")
+	public String hi() {
+		return "Hi DK";
+	}
 }
